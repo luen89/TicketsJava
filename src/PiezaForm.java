00d1b02f5a492d1b9a0d48c2c3efd4da6c2d47f6
Jesús Toledo <109049666+jeatog@users.noreply.github.com>
2023-01-18 18:55:24 -0600

    Issue 5 resuelto: Condiciones en costo

--- src/PiezaForm.java
+++ src/PiezaForm.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,102 +18,107 @@ import java.util.Arrays;
  * @author luen89
  */
 
-public class PiezaForm extends JPanel implements ChangeListener{
-       private JLabel lbNPiezas, lbAcero , lbpeso;
-       private String Servicio[] = {"Selecciona Servicio","Pavonado","Templado","Templado y Pavonado"};
-       private String acero[] = {"Selecciona tipo", "4140", "8620", "1045","Colled Rolled","Q1","D2"};
-       private SpinnerModel smNpiezas;
-       private JSpinner sNpiezas;
-       private SpinnerModel smNkilos;
-       private JSpinner sNKilos;
-       private JComboBox cbServicios, cbAcero;
-       private Elemento elemento;
+public class PiezaForm extends JPanel implements ChangeListener {
+     private JLabel lbNPiezas, lbAcero, lbpeso;
+     private String Servicio[] = { "Selecciona Servicio", "Pavonado", "Templado", "Templado y Pavonado" };
+     private String acero[] = { "Selecciona tipo", "4140", "8620", "1045", "Colled Rolled", "Q1", "D2" };
+     private SpinnerModel smNpiezas;
+     private JSpinner sNpiezas;
+     private SpinnerModel smNkilos;
+     private JSpinner sNKilos;
+     private JComboBox cbServicios, cbAcero;
+     private Elemento elemento;
 
+     public PiezaForm() {// Constructor
+          super(new FlowLayout(FlowLayout.LEFT));
+          // Crea Objeto Elemento
+          elemento = new Elemento("", "", 0, 0.0);
 
-       public PiezaForm(){// Constructor
-           super(new FlowLayout(FlowLayout.LEFT));
-           //Crea Objeto Elemento
-           elemento=new Elemento("", "", 0, 0.0);
-
-           //Inicializa la ComboBox de Servicios
-           cbServicios = new JComboBox(Servicio);
-           cbServicios.addActionListener(new ActionListener () {                         
+          // Inicializa la ComboBox de Servicios
+          cbServicios = new JComboBox(Servicio);
+          cbServicios.addActionListener(new ActionListener() {
                @Override
                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Entra al action del servicio");
                     elemento.setServicio(cbServicios.getSelectedItem().toString());
                     cbAcero.setSelectedItem(0);
-                    switch(cbServicios.getSelectedIndex()){
+                    switch (cbServicios.getSelectedIndex()) {
                          case 1:
-                              cbAcero.setModel(new DefaultComboBoxModel<>( acero ));
+                              cbAcero.setModel(new DefaultComboBoxModel<>(acero));
                               cbAcero.setEnabled(true);
                               break;
                          case 2, 3:
-                              cbAcero.setModel(new DefaultComboBoxModel<>( 
-                                   Arrays.stream(acero).filter(x -> x != "Colled Rolled").toArray()
-                              ));
+                              cbAcero.setModel(new DefaultComboBoxModel<>(
+                                        Arrays.stream(acero).filter(x -> x != "Colled Rolled").toArray()));
                               cbAcero.setEnabled(true);
                               break;
                          default:
-                              cbAcero.setEnabled(false);                         
+                              cbAcero.setEnabled(false);
                     }
                }
-           });
+          });
 
-           //Inicializa la ComboBox de Aceros
-           cbAcero = new JComboBox(acero);
-           cbAcero.addActionListener(new ActionListener () {                         
+          // Inicializa la ComboBox de Aceros
+          cbAcero = new JComboBox(acero);
+          cbAcero.addActionListener(new ActionListener() {
                @Override
                public void actionPerformed(ActionEvent e) {
-                    elemento.setAcero(cbAcero.getSelectedItem().toString()); } });
+                    elemento.setAcero(cbAcero.getSelectedItem().toString());
+               }
+          });
           cbAcero.setEnabled(false);
 
-          //Inicializa las etiquetas
+          // Inicializa las etiquetas
           lbNPiezas = new JLabel("Numero de Piezas");
-          lbAcero= new JLabel("       Acero");
-          lbpeso= new JLabel("        Peso en Kg");
-          
-          //Inicializa el Spinner de Piezas
-          smNpiezas = new SpinnerNumberModel(0,0,100,1);
+          lbAcero = new JLabel("       Acero");
+          lbpeso = new JLabel("        Peso en Kg");
+
+          // Inicializa el Spinner de Piezas
+          smNpiezas = new SpinnerNumberModel(0, 0, 100, 1);
           sNpiezas = new JSpinner(smNpiezas);
           sNpiezas.addChangeListener(this);
-          
-          //Inicializa el Spinner de Kilos
-          smNkilos = new SpinnerNumberModel(0.0,0.0,100.0,0.1);
+
+          // Inicializa el Spinner de Kilos
+          smNkilos = new SpinnerNumberModel(0.0, 0.0, 100.0, 0.1);
           sNKilos = new JSpinner(smNkilos);
           sNKilos.addChangeListener(this);
-          }
-
-     
-     public void preDisplay(){
-          this.add(cbServicios);
-            this.add(lbNPiezas);
-            this.add(sNpiezas);
-            this.add(lbAcero);
-            this.add(cbAcero);
-            this.add(lbpeso);
-            this.add(sNKilos);
      }
 
+     public void preDisplay() {
+          this.add(cbServicios);
+          this.add(lbNPiezas);
+          this.add(sNpiezas);
+          this.add(lbAcero);
+          this.add(cbAcero);
+          this.add(lbpeso);
+          this.add(sNKilos);
+     }
 
-          public void sumaKilos(Double kilos){
-               //
-          }
-          public void sumaPieza(Integer piezas){
-               //
-          }
+     public void sumaKilos(Double kilos) {
+          //
+     }
 
-          @Override
-          public void stateChanged(ChangeEvent e) {
-               if (e.getSource()==sNpiezas){
-                    elemento.setPiezas(Integer.parseInt(sNpiezas.getValue().toString()));
-               }
-               if (e.getSource()==sNKilos){
-                    elemento.setKilos(Double.parseDouble(sNKilos.getValue().toString()));
-               }
-               System.out.println(elemento.getAcero());
-               System.out.println(elemento.getPiezas());
-               System.out.println(elemento.getKilos());
-               System.out.println(elemento.getServicio());          
-               }
-    
+     public void sumaPieza(Integer piezas) {
+          //
+     }
+
+     @Override
+     public void stateChanged(ChangeEvent e) {
+          if (e.getSource() == sNpiezas) {
+               elemento.setPiezas(Integer.parseInt(sNpiezas.getValue().toString()));
+          }
+          if (e.getSource() == sNKilos) {
+               elemento.setKilos(Double.parseDouble(sNKilos.getValue().toString()));
+          }
+          System.out.println(elemento.getAcero());
+          System.out.println(elemento.getPiezas());
+          System.out.println(elemento.getKilos());
+          System.out.println(elemento.getServicio());
+     }
+
+     public Elemento getElemento(){
+          return elemento;
+     }
+     
+
 }
