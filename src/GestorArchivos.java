8485c2eb2454ff6d802235e4488ec0d848dbcef6
abiudsal <abiud.salazar@alumno.buap.mx>
2023-01-27 09:14:21 -0600

    correccion DataInputStream obsoleta

--- src/GestorArchivos.java
+++ src/GestorArchivos.java
@@ -1,11 +1,13 @@
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StreamTokenizer;
@@ -202,8 +204,9 @@ public class GestorArchivos {
         String[] st;
         ArrayList<EntradaRegistro> registros = new ArrayList<EntradaRegistro>();
         //BufferedReader in = new BufferedReader(new FileReader(f));    
-        try{            
-            DataInputStream in = new DataInputStream(new FileInputStream(f));   
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(f)));            
+            // DataInputStream in = new DataInputStream(new FileInputStream(f));   
 
             String line;
             while ((line = in.readLine()) != null){
