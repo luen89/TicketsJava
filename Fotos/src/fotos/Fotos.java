67abaf43577a1d7462fd6f05dcbc17cf1df1dc6e
Juan Antonio Frias <antonio.frias@emeefe.mx>
2023-02-16 19:42:51 -0600

    prueba fotos

---
+++ Fotos/src/fotos/Fotos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fotos;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+/**
+ *
+ * @author jafa0
+ */
+public class Fotos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmFoto.main(args);
+        
+    }
+    
+}
